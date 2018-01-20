91bd6a80014563057d5e1113e94d2dae9dfc24de
Przemek Galdys <galdys.przemek@gmail.com>
2018-01-20 21:44:58 +0100

    country-app part 2

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import {render} from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store/index';
+import routes from './routes';
 import DevTools from './DevTools';
-import { getCountries } from './actions/actions-countries';
 
 render(
     <Provider store={store}>
         <div>
-            <h1> Welcome </h1>
+            <Router history={hashHistory} routes={routes} />
             <DevTools />
         </div>
     </Provider>, 
     document.getElementById('root')
 );
-
-store.dispatch(getCountries());
---
+++ src/presentational/contact.component.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Contact = () => (
+    <h1>Ulica: Błotna 14</h1>
+);
+
+export default Contact;
